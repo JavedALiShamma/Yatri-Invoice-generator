7479f1aadd7b00ac97146c3ce2d3b4de60969082
Javed Ali <javedshamma@gmail.com>
2024-03-04 17:22:03 +0530

    Naya h bhai

--- script.js
+++ script.js
@@ -2,15 +2,70 @@
 let form=document.getElementById("main-form");
 form.addEventListener("submit",function(e){
     e.preventDefault();
+    /// Here we are extracting color of the vehicle 
+
     let colorName=form.color.value;
-    console.log(colorName);
-    let modelName=form.modelName.value;
-    console.log(modelName);
+    let ProcuctcolorName=document.getElementById("colorname");
+    ProcuctcolorName.innerHTML=colorName;
+    
+    
+    if(form.newElement){
+        let modelName=form.newElement.value;
+        let vehicaleName=document.getElementById("vehicle-name");
+        vehicaleName.innerHTML=modelName;
+    }else if(!form.newElement){
+        let vehicaleName1=document.getElementById("vehicle-name");
+        // vehicaleName1.classList.add="display-off";
+        let modelName=form.modelName.value;
+        let vehicaleName=document.getElementById("vehicle-name");
+        vehicaleName.innerHTML=modelName;
+    }
+    // else{
+    //     let modelName=form.modelName.value;
+    //     let vehicaleName=document.getElementById("vehicle-name");
+    //     vehicaleName.innerHTML=modelName;
+    // }
+    // Here we need to extract color of the vehicle also
+
+
     let price=form.price.value;
+    let productPrice=document.getElementById("product-price");
+    productPrice.innerHTML=price;
     let quantity1=form.qunatityPro.value;
-    console.log(price*quantity1);
+    let quantitynumber=document.getElementById("quantity-number");
+    quantitynumber.innerHTML=quantity1;
+
+   let totalAmount=price*quantity1;
+   let totalAmoutPrice=document.getElementById("total-amount");
+   totalAmoutPrice.innerHTML=totalAmount;
     //// here we need to add all the elemnts in the given table
     let srNo=1;
-    let productName=document.getElementById("product-name1");
-    productName.innerHTML=modelName;
+    // Here we need to calculate the gross total
+    let grosstotal=[];
+    grosstotal.push(totalAmount);
+    console.log(grosstotal);
+    let sumGross=0;
+    for(let i=0;i<grosstotal.length;i++){
+        sumGross+=grosstotal[i];
+    }
+    // Here we have the gross total now we can add the gross total of the elements
+    let grosstotalAmount=document.getElementById("grosstotal");
+    grosstotalAmount.innerHTML=sumGross;
 });
+// here we need to add the find this element
+let modelName=document.getElementById("modelName");
+modelName.addEventListener("click",function(e){
+    if(e.target.value=="other")addNewField();
+})
+function addNewField(){
+    let newAddElement=document.getElementById("new-add-element");
+    newAddElement.innerHTML=`<label for="newElement"> Product-Name</label>
+    <input type="text" name="newElement" id="newModel">`
+    // let newModel=document.getElementById("newModel");
+    // newModel.addEventListener("change",function(){
+    //     // here we need to catch the change
+    //     let changes=newModel.innerText;
+    //     console.log(changes);
+
+    // })
+}   
